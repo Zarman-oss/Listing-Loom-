f7897c3e922d103904fc72482dbd4adb67e1fd19
z31430158@gmail.com <zarmanimran@gmail.com>
2024-06-16 08:22:43 +0500

    almost done commit 4

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,12 @@ const nextConfig = {
       },
     ],
   },
+  experimental: {
+    serverActions: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
