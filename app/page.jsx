556a5d898e069750d70e178f8fb39ad8fdb990a9
z31430158@gmail.com <zarmanimran@gmail.com>
2024-06-22 04:07:17 +0500

    almost done commit 37

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,8 @@ import FeaturedProperties from '../components/FeaturedProperties';
 import Hero from '../components/Hero';
 import InfoBoxes from '../components/infoBoxes';
 
+export const dynamic = 'force-dynamic';
+
 export default function page() {
   return (
     <>
